5a1ed8ba6cb0453e2f136a59688bf9e92a58a7fe
Greg1992 <yatesgregory92@gmail.com>
2018-09-01 04:32:02 +0100

    testing fix

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-var env = process.env.NODE_ENV || 'development';
+var env = process.env.NODE_ENV || 'production';
 console.log('env *****************', env)
 if(env === 'production'){
   console.log('no')
